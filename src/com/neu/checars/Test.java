477ebc5a532359ac847b3a289972cc555635dd08
checars <che_cars@126.com>
2016-11-02 21:44:23 +0800

    commit

--- src/com/neu/checars/Test.java
+++ src/com/neu/checars/Test.java
@@ -1,20 +1,31 @@
 package com.neu.checars;
 
-public class Test {
+import java.util.*;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(Fibonacci(0));
-	}
-	public static int Fibonacci(int n) {
-		if(n == 0) return 0;
-		if(n == 1 || n == 2) return 1;
-        else return Fibonacci(n-1) + Fibonacci(n-2);
+public class Test{
+	public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String s = in.nextLine();
+           
+            int character = 0, blank = 0, digit = 0, other = 0;
+            for(int i = 0; i < s.length(); i ++){
+                char c = s.charAt(i);
+                if((c>='a' && c<='z') || (c>='A' && c<='Z'))
+                    character ++;
+                else if(c == ' ')
+                    blank ++;
+                else if(c>='0' && c<='9')
+                    digit ++;
+                else
+                    other ++;
+            }
+            
+            System.out.println(character);
+            System.out.println(blank);
+            System.out.println(digit);
+            System.out.println(other);
+        }
     }
-	 public int RectCover(int target) {
-			return (int)Math.pow(2,target/2);
-	    }
+    
 }
